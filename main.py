c329b76d6d4ad659f351b5388f6c83ed991d0805
Alex QAsper <semidog@gmail.com>
2022-11-01 22:27:28 +0500

    try to add cenzor func

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-import os
+import json, string
+
 
 TOKEN = "5732215283:AAEK4rDutfCc6OPsXZZPFGw0CEITWTV5UiE"
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+
 
 async def on_start_up(_):
     print('Bot online')
@@ -22,13 +24,16 @@ async def command_start(message: types.message):
     except:
         await message.reply('Общение с ботом через ЛС, напишите ему \n https://t.me/adsf1234bot')
 
+
 @dp.message_handler(commands=['Расположение'])
 async def place(message: types.message):
     await message.reply("Батуми, Батумский бульвар, дом 13")
 
+
 @dp.message_handler(commands=['Время'])
 async def work_hour_start(message: types.message):
     await message.reply("Пн.8-12 \n Вт. 8-13 \n Ср. 9-16 \n Чт. 10-14 \n Пт. 12-18")
+
 
 """ Админская часть """
 
@@ -37,9 +42,11 @@ async def work_hour_start(message: types.message):
 
 @dp.message_handler()
 async def echo_send(message: types.Message):
-    # await message.answer(message.text)
-    await message.reply(message.text)
-    # await bot.send_message(message.from_user.id, message.text)
+    if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split(' ')}\
+        .intersection(set(json.load(open('cenz.json')))):
+        await message.reply("Мат запрещён!")
+        await message.delete()
+
 
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_start_up)
